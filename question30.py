de3d4267a48f838fc5b312dea594646bc3951147
Himanshu Tomar <himanshutomar3031@gmail.com>
2022-06-19 00:28:00 +0530

    Regex

--- question30.py
+++ question30.py
@@ -4,3 +4,9 @@ email = "john@google.com elise@python.com"
 pattern = "(\w+)@\w+.com"
 ans = re.match(pattern, email)
 print(ans.group(1))
+
+
+email = "john@google.com elise@python.com"
+pattern = "\w+@(\w+).com"
+ans = re.findall(pattern, email)
+print(ans)
